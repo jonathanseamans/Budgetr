161e45a6a26fc84bea2b5c99cbae060a9ee58bf9
unknown <hschueler@DTTS81E012.us.oneilsoft.com>
2014-04-07 10:09:23 -0700

    tests1

--- test.php
+++ test.php
@@ -1,14 +1,25 @@
 <!doctype html>
 <html lang="en">
 <style>
-body { background: url(resources/unnamed.png) no-repeat #f6f1b9 fixed top right; 
-	   padding-right: 250px; 
-	 }
+body { background: url(resources/unnamed.png) ; 
+}
+#hea1 {
+	background-color:white;
+}
+input
+{
+  -moz-border-radius: 15px;
+ border-radius: 15px;
+    border:solid 4px black; 
+    padding:5px;
+    font-family: sans-serif;
+    font-size: 20;
+}
+	</style>
+	}
 <body>
-
-	<h1>Welcome to your PHP application on OpenShift</h1>
-
-
+	<h1 id="hea1">Welcome to your PHP application on OpenShift</h1>
+	<input type="text" />
 
 </body>
 </html>
