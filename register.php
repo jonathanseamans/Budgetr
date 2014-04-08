a441f4512391e86e5202ff15ff121b54c3f506a6
unknown <hschueler@DTTS81E012.us.oneilsoft.com>
2014-04-08 10:06:21 -0700

    more fixes

--- register.php
+++ register.php
@@ -3,53 +3,63 @@
 <?php
 	if(isset($_POST['add'])) {
 
+		$errorcount = false;
 		$user_email = $_POST['user_email'];
 		$user_password = $_POST['user_password'];
 		$user_password2 = $_POST['user_password2'];
-
+		// Validation Begin
 		if (empty($user_email))
-     		{	$emailErr = "Email is required";}
+     		{	
+     			$emailErr = "Email is required";
+     			$errorcount = true;
+     		}
    		else
      		{	$user_email = test_input($user_email);}
 
      	if (empty($user_password))
-     		{	$passwordErr = "Password is required";}
-   		// else
-     // 		{	
-     // 			if (empty($user_password2))
-     // 				{	$password2Err = "Password is required";}
-   		// 		else
-     // 				{	
-     // 					if ($user_password != $user_password2))
-     // 						{	$paswordErr2 = "Passwords must match";}
-   		// 				else
-     // 						{	$user_password = test_input($user_password);}
-     // 				}
-     // 		}
-	
-     
-			
-		$dbhost = $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
-		$dbuser = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-		$dbpass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-		$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-	
-		if(! $conn )
-		{
-		  die('Unable to Connect to Server' . mysql_error());
-		}
+     		{	
+     			$passwordErr = "Password is required";
+     			$errorcount = true;
+     		}
+   		else
+     		{	$user_password = test_input($user_password);}
 
-		$sql = "INSERT INTO user ".
-		       "(user_id,user_email, user_password) ".
-		       "VALUES('NULL','$user_email','$user_password')";
-		mysql_select_db('test_db');
-		$retval = mysql_query( $sql, $conn );
-		if(! $retval )
-		{
-		  die('Could not enter data: ' . mysql_error());
+     	if(empty($user_password2))
+     	{
+     		$password2Err = "Password is required";
+     		$errorcount = true;
+     	}
+     	else
+     		{ $user_password2 = test_input($user_password2); }
+
+     	if($user_password2 != $user_password) {
+     		$errorcount = true;
+     	}
+		// Validation End
+     	if(!errorcount) {
+			
+			$dbhost = $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
+			$dbuser = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
+			$dbpass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
+			$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+		
+			if(! $conn )
+			{
+			  die('Unable to Connect to Server' . mysql_error());
+			}
+
+			$sql = "INSERT INTO user ".
+			       "(user_id,user_email, user_password) ".
+			       "VALUES('NULL','$user_email','$user_password')";
+			mysql_select_db('test_db');
+			$retval = mysql_query( $sql, $conn );
+			if(! $retval )
+			{
+			  die('Could not enter data: ' . mysql_error());
+			}
+			echo "Account Registered successfully\n";
+			mysql_close($conn);
 		}
-		echo "Account Registered successfully\n";
-		mysql_close($conn);
 	}
 	function test_input($data)
 	{
@@ -60,28 +70,34 @@
 	}
 ?>
 <style>
-body { background: url(resources/unnamed.png) ; 
-}
-#hea1 {
-	background-color:white;
-}
-input
-{
-  -moz-border-radius: 15px;
- border-radius: 15px;
-    border:solid 4px black; 
-    padding:5px;
-    font-family: sans-serif;
-    font-size: 20;
-}
-div{
-background-color: rgba(5,4,2,0.2);
-width:850px;
-height: 100%;
-margin-left:auto;
-margin-right:auto;
-padding: 15px; }
-	</style>
+	.error {color: #FF0000;}
+
+	body { background: url(resources/unnamed.png) ; 
+	}
+
+	#hea1 {
+		background-color:white;
+	}
+
+	input
+	{
+	  -moz-border-radius: 15px;
+	 border-radius: 15px;
+	    border:solid 4px black; 
+	    padding:5px;
+	    font-family: sans-serif;
+	    font-size: 20;
+	}
+
+	div{
+	background-color: rgba(5,4,2,0.2);
+	width:850px;
+	height: 100%;
+	margin-left:auto;
+	margin-right:auto;
+	padding: 15px; }
+	
+</style>
 <body>
 	<div> 
 		<form method="post" action="<?php $_PHP_SELF ?>">
