de1c6b2301b95835adc1014113e38548642a7dff
HSchueler <buluen@gmail.com>
2014-04-07 20:01:12 -0800

    registerfix

--- register.php
+++ register.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <?php
 	if(isset($_POST['add']))
-	{
+		if($user_password2 != $user_password) {
+			echo "Password not the same in both fields";
+		}		
 		$dbhost = $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
 		$dbuser = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
 		$dbpass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
@@ -17,8 +19,8 @@
 		$user_password = $_POST['user_password'];
 
 		$sql = "INSERT INTO user ".
-		       "(user_id,user_email, user_password) ".
-		       "VALUES('$user_id','$user_email',$user_password)";
+		       "(user_id,user_id,user_email, user_password) ".
+		       "VALUES('NULL','$user_id','$user_email',$user_password)";
 		mysql_select_db('test_db');
 		$retval = mysql_query( $sql, $conn );
 		if(! $retval )
