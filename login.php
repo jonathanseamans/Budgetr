b2f7e2b410e2e388d4a946296b71a5cbd45110a5
unknown <hschueler@DTTS81E012.us.oneilsoft.com>
2014-04-09 15:14:28 -0700

    login

---
+++ login.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<style>
+body { background: url(resources/unnamed.png) ; 
+}
+#hea1 {
+	background-color:white;
+}
+input
+{
+  -moz-border-radius: 15px;
+ border-radius: 15px;
+    border:solid 4px black; 
+    padding:5px;
+    font-family: sans-serif;
+    font-size: 20;
+}
+div{
+background-color: rgba(5,4,2,0.2);
+width:850px;
+height: 100%;
+margin-left:auto;
+margin-right:auto;
+padding: 15px; }
+	</style>
+<body>
+	<div> 
+		<p>Log In</p>
+		<br />
+		<br />
+	</div>
+
+</body>
+</html>
