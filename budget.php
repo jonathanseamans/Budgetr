8c45c6c9195504406769a4cf77a2a2c49054dd72
HSchueler <buluen@gmail.com>
2014-04-11 10:55:04 -0700

    load fix2

--- budget.php
+++ budget.php
@@ -1,12 +1,18 @@
 <html>
 <?php
 	session_start();
-
+	echo "file loaded successfully";
 	include 'mysql.php';
 			
 	$sql = "SELECT * FROM budget WHERE uid='$_SESSION['userid']'";
 	$result = mysql_query($sql, $conn);
 	$count=mysql_num_rows($result);
+?>
+<br />
+<p>
+<?php 
+	echo "test 2";
+	echo $count;
 	for ($i=0; $i<=$count; $i++)
   	{
 	  	if(mysql_result($result, $i,'type') == 2) {
@@ -26,6 +32,8 @@
     // 	echo "Call Wizard";
     // }
 ?>
+</p>
+<br />
 <style><?php include 'css/main.css'; ?></style>
 <p>
 	<?php 
