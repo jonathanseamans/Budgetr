670bc42bafd274a30d1a779943d8d9d03b224fdb
James Harrison Schueler <buluen@gmail.com>
2014-04-10 18:43:37 -0700

    loading budget

--- budget.php
+++ budget.php
@@ -1,15 +1,8 @@
 <html>
-	<?php echo "Loading Please Wait"; ?>
+	<?php echo "Loading Your Budget Please Wait"; ?>
 	<style>
 		<?php include 'css/loading.css'; ?>
 	</style>
 	<div class="ball"></div>
-	<div class="ball1"></div>
-<!--	--><?php //sleep(5);
-//			echo "Loading Complete";
-//	script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-//	</script>
-//	<script>
-//		$("link[href='css/loading.css']").remove();
-//	</script> ?>
+	<?php sleep(5); ?>
 </html>
