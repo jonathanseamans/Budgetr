ad7cb9867b5b6624d2e7ca8ff8fd28d76c507e33
HSchueler <buluen@gmail.com>
2014-04-11 15:12:03 -0700

    tests7

--- test2.php
+++ test2.php
@@ -1,14 +1,11 @@
 <?php
-define('INCLUDE_CHECK',true);
+//provide your hostname, username and dbname
 include 'mysql.php';
-
-$userID=$_GET["userID"];
-$sql="SELECT * FROM budget WHERE uid = '2'";
+$book_name = $_POST['book_name'];
+$sql = "select * from budget where uid LIKE '$book_name%'";
 $result = mysql_query($sql);
-
 while($row=mysql_fetch_array($result))
 {
 echo "<p>".$row['UDT']."</p>";
 }
-
 ?>
