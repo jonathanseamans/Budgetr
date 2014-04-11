4b0b104b8d0d54c3715145207eb3174ccbdc71c3
James Harrison Schueler <buluen@gmail.com>
2014-04-10 19:13:15 -0700

    loading

--- index.php
+++ index.php
@@ -1,26 +1,6 @@
 <!doctype html>
 <html lang="en">
-<style>
-body { background: url(resources/unnamed.png) ; 
-}
-
-input
-{
-  -moz-border-radius: 15px;
- border-radius: 15px;
-    border:solid 4px black; 
-    padding:5px;
-    font-family: sans-serif;
-    font-size: 20;
-}
-div{
-background-color: rgba(5,4,2,0.2);
-width:850px;
-height: 100%;
-margin-left:auto;
-margin-right:auto;
-padding: 15px; }
-	</style>
+<link rel="stylesheet" href="/css/main.css" type="text/css">
 <body>
 	<div> 
 		<p>Register Here</p>
