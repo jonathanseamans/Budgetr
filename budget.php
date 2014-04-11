1e95021dfbfe8f6eae7e753e737caf4c0474094a
James Harrison Schueler <buluen@gmail.com>
2014-04-10 18:33:48 -0700

    loading budget

--- budget.php
+++ budget.php
@@ -1,8 +1,15 @@
-<?php
-echo '<p>Starting request 1</p>';
-flush();
-sleep(2);
-echo '<p>Request 1 done. Starting request 2.</p>';
-flush();
-sleep(2);
-echo '<p>Request 2 done.</p>';
+<html>
+	<?php echo "Loading Please Wait"; ?>
+	<style>
+		<?php include 'css/loading.css'; ?>
+	</style>
+	<div class="ball"></div>
+	<div class="ball1"></div>
+	<?php sleep(5);
+			echo "Loading Complete";
+	script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
+	</script>
+	<script>
+		$("link[href='css/loading.css']").remove();
+	</script> ?>
+</html>
