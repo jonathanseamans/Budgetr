570f5b42d490bfc2aa974c87799bd21fb5187063
James Harrison Schueler <buluen@gmail.com>
2014-04-10 18:05:42 -0700

    loading budget

--- budget.php
+++ budget.php
@@ -1,7 +1,8 @@
+<?php
 echo '<p>Starting request 1</p>';
 flush();
 sleep(2);
 echo '<p>Request 1 done. Starting request 2.</p>';
 flush();
 sleep(2);
-echo '<p>Request 2 done.</p>';  
+echo '<p>Request 2 done.</p>';
