8432e040f28f5b510cc1c8d238d5f35133991302
HSchueler <buluen@gmail.com>
2014-04-11 14:18:20 -0700

    tests5

--- test.php
+++ test.php
@@ -1,46 +1,57 @@
-<html>
-<head>
-<script>
-function showUser(str)
-{
-if (str=="")
-  {
-  document.getElementById("txtHint").innerHTML="";
-  return;
-  } 
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
-    }
-  }
-xmlhttp.open("GET","test2.php?q="+str,true);
-xmlhttp.send();
-}
-</script>
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<meta charset="utf-8"> 
+<title>User interface for Ajax, PHP, MySQL demo</title> 
+<meta name="description" content="HTML code for user interface for Ajax, PHP and MySQL demo."> 
+<link href="../includes/bootstrap.css" rel="stylesheet">
+<style type="text/css">
+body {padding-top: 40px; padding-left: 25%}
+li {list-style: none; margin:5px 0 5px 0; color:#FF0000}
+</style>
 </head>
 <body>
-
-<form>
-<select name="users" onchange="showUser(this.value)">
-<option value="">Select a person:</option>
-<option value="1">Peter Griffin</option>
-<option value="2">Lois Griffin</option>
-<option value="3">Glenn Quagmire</option>
-<option value="4">Joseph Swanson</option>
-</select>
+<form class="well-home span6 form-horizontal" name="ajax-demo" id="ajax-demo">
+<div class="control-group">
+              <label class="control-label" for="book">Book</label>
+              <div class="controls">
+                <input type="text" id="book" onKeyUp="book_suggestion()">
+				<div id="suggestion"></div>
+			 </div>
+ </div>
+ <div class="control-group">
+              <div class="controls">
+                <button type="submit" class="btn btn-success">Submit</button>
+              </div>
+ </div>
 </form>
-<br>
-<div id="txtHint"><b>Person info will be listed here.</b></div>
-
+<script>
+function book_suggestion()
+{
+var book = document.getElementById("book").value;
+var xhr;
+ if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    xhr = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE 8 and older
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+}
+var data = "book_name=" + book;
+     xhr.open("POST", "test2.php", true); 
+     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                  
+     xhr.send(data);
+	 xhr.onreadystatechange = display_data;
+	function display_data() {
+	 if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+       //alert(xhr.responseText);	   
+	  document.getElementById("suggestion").innerHTML = xhr.responseText;
+      } else {
+        alert('There was a problem with the request.');
+      }
+     }
+	}
+}
+</script>
 </body>
 </html>
+
--- test2.php
+++ test2.php
@@ -1,30 +1,10 @@
 <?php
-$q = intval($_GET['q']);
-
 include 'mysql.php';
-$sql="SELECT * FROM budget WHERE uid = '$q'";
-
-$result = mysql_query($con,$sql);
-
-echo "<table border='1'>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-<th>Age</th>
-<th>Hometown</th>
-<th>Job</th>
-</tr>";
-
-while($row = mysql_fetch_array($result))
-  {
-  echo "<tr>";
-  echo "<td>" . $row['UDT'] . "</td>";
-  echo "<td>" . $row['UDV'] . "</td>";
-  echo "<td>" . $row['Age'] . "</td>";
-  echo "<td>" . $row['Hometown'] . "</td>";
-  echo "<td>" . $row['Job'] . "</td>";
-  echo "</tr>";
-  }
-echo "</table>";
-
+$book_name = $_POST['book_name'];
+$sql = "select * from budget where uid LIKE '$book_name%'";
+$result = mysql_query($sql);
+while($row=mysql_fetch_array($result))
+{
+echo "<p>".$row['UDT']."</p>";
+}
 ?>
