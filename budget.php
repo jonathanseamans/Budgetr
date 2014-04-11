1ab79bbb703e1bdee57f9006c24094121043273c
James Harrison Schueler <buluen@gmail.com>
2014-04-10 18:36:08 -0700

    loading budget

--- budget.php
+++ budget.php
@@ -5,11 +5,11 @@
 	</style>
 	<div class="ball"></div>
 	<div class="ball1"></div>
-	<?php sleep(5);
-			echo "Loading Complete";
-	script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-	</script>
-	<script>
-		$("link[href='css/loading.css']").remove();
-	</script> ?>
+<!--	--><?php //sleep(5);
+//			echo "Loading Complete";
+//	script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
+//	</script>
+//	<script>
+//		$("link[href='css/loading.css']").remove();
+//	</script> ?>
 </html>
