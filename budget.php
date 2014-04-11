26458a61f2294a3bfbfb7cc4a63192fab2adb3ad
James Harrison Schueler <buluen@gmail.com>
2014-04-10 17:57:17 -0700

    loading budget

--- budget.php
+++ budget.php
@@ -1,3 +1,16 @@
-<?php
-echo HII;
-?>
+<HTML>
+<BODY>
+<DIV ID="flushme">
+	Hello, world!
+</DIV>
+<?php flush(); sleep(2); ?>
+<SCRIPT>
+	d = document.getElementById("flushme");
+	d.innerHTML = "Goodbye, Perl!";
+</SCRIPT>
+<?php flush(); sleep(2); ?>
+<SCRIPT>
+	d.innerHTML = "Goodnight, New York!";
+</SCRIPT>
+</BODY>
+</HTML>
