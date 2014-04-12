0a2b72d3ef39b95e71df628fa4d904e841de7c14
James Harrison Schueler <buluen@gmail.com>
2014-04-12 00:03:44 -0700

    Insert Title

--- wizard2.php
+++ wizard2.php
@@ -1,9 +1,9 @@
 <?php
 include 'mysql.php';
-$title_name = $_POST['title_name'];
-$uuid = $_SESSION['userid'];
-// type 2 == title
-$sql = "INSERT INTO budget (uid,bid,type,UDT) VALUES ('$uuid','1','2','$title_name')";
-mysql_query($sql);
+//$title_name = $_POST['title_name'];
+//$uuid = $_SESSION['userid'];
+//// type 2 == title
+//$sql = "INSERT INTO budget (uid,bid,type,UDT) VALUES ('$uuid','1','2','$title_name')";
+//mysql_query($sql);
 echo "title changed!";
 ?>
