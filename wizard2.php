cbe5514414aaa96a220bbc21622009a787533939
James Harrison Schueler <buluen@gmail.com>
2014-04-11 23:38:19 -0700

    Insert Title

--- wizard2.php
+++ wizard2.php
@@ -4,9 +4,5 @@ include 'mysql.php';
 $title_name = $_POST['title_name'];
 // type 2 == title
 $sql = "INSERT INTO budget (uid,bid,type,UDT) VALUES ($_SESSION['userid'],1,2,$title_name)";
-$result = mysql_query($sql);
-while($row=mysql_fetch_array($result))
-{
-echo "<p>".$row['UDT']."</p>";
-}
+mysql_query($sql);
 ?>
