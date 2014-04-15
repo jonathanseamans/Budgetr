ded77eaa5365c30d97e54db7fc8d9bb878e076c4
HSchueler <buluen@gmail.com>
2014-04-15 10:31:46 -0700

    final calendar

---
+++ calendar.php
@@ -0,0 +1,37 @@
+ <?php 
+    function date_picker($name, $startyear=NULL, $endyear=NULL)
+{
+    if($startyear==NULL) $startyear = date("Y")-5;
+    if($endyear==NULL) $endyear=date("Y")+25; 
+
+    $months=array('','January','February','March','April','May',
+    'June','July','August', 'September','October','November','December');
+
+    // Month dropdown
+    $html="<select name=\"".$name."month\">";
+
+    for($i=1;$i<=12;$i++)
+    {
+       $html.="<option value='$i'>$months[$i]</option>";
+    }
+    $html.="</select> ";
+   
+    // Day dropdown
+    $html.="<select name=\"".$name."day\">";
+    for($i=1;$i<=31;$i++)
+    {
+       $html.="<option $selected value='$i'>$i</option>";
+    }
+    $html.="</select> ";
+
+    // Year dropdown
+    $html.="<select name=\"".$name."year\">";
+
+    for($i=$startyear;$i<=$endyear;$i++)
+    {      
+      $html.="<option value='$i'>$i</option>";
+    }
+    $html.="</select> ";
+
+    return $html;
+}
--- wizard1.php
+++ wizard1.php
@@ -4,43 +4,8 @@
 <br>
 <p> Set the starting date for your budget
 <?php
-	  function date_picker($name, $startyear=NULL, $endyear=NULL)
-{
-    if($startyear==NULL) $startyear = date("Y")-100;
-    if($endyear==NULL) $endyear=date("Y")+50; 
-
-    $months=array('','January','February','March','April','May',
-    'June','July','August', 'September','October','November','December');
-
-    // Month dropdown
-    $html="<select name=\"".$name."month\">";
-
-    for($i=1;$i<=12;$i++)
-    {
-       $html.="<option value='$i'>$months[$i]</option>";
-    }
-    $html.="</select> ";
-   
-    // Day dropdown
-    $html.="<select name=\"".$name."day\">";
-    for($i=1;$i<=31;$i++)
-    {
-       $html.="<option $selected value='$i'>$i</option>";
-    }
-    $html.="</select> ";
-
-    // Year dropdown
-    $html.="<select name=\"".$name."year\">";
-
-    for($i=$startyear;$i<=$endyear;$i++)
-    {      
-      $html.="<option value='$i'>$i</option>";
-    }
-    $html.="</select> ";
-
-    return $html;
-}
-echo date_picker("registration"); 
+	include 'calendar.php';
+	echo date_picker("calendar"); 
 ?>
 </p>
 <br />
