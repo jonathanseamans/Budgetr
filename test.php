16b881e4e6af9307f0620b1ca3e8e72903173e20
HSchueler <buluen@gmail.com>
2014-04-18 15:15:34 -0700

    test

--- test.php
+++ test.php
@@ -1,34 +1,16 @@
 <html>
-<style>
-#overlay {
-     visibility: hidden;
-     position: absolute;
-     left: 0px;
-     top: 0px;
-     width:100%;
-     height:100%;
-     text-align:center;
-     z-index: 1000;
-}
-#overlay div {
-     width:300px;
-     margin: 100px auto;
-     background-color: #fff;
-     border:1px solid #000;
-     padding:15px;
-     text-align:center;
-}
-</style>
-<div id="overlay">
-     <div>
-          <p>Content you want the user to see goes here.</p>
-     </div>
-</div>
-<script>
-function overlay() {
-  el = document.getElementById("overlay");
-  el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-}
-</script>
-
+<script type="text/javascript" src="jquery/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="jquery/jquery.easyModal.js"></script>
+<body>
+  <button id="open"> test </button>
+  <div id="test">
+  <p> hello </p>
+  </div>
+</body>
 </html>
+<script>
+$(function() {
+    $("#test").easyModal();
+});
+$("#open").trigger('openModal');
+</script>
