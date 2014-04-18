57683e6bdc17d64a3a212c92c549077db7a40a61
HSchueler <buluen@gmail.com>
2014-04-18 14:57:56 -0700

    test

--- test.php
+++ test.php
@@ -1,68 +1,34 @@
 <html>
-<div id="panel">
-  <input type="button" class="button" value="1" id="btn1">
-  <input type="button" class="button" value="2" id="btn2">
-  <input type="button" class="button" value="3" id="btn3">
-  <br>
-  <input type="text" id="valueFromMyModal">
-  <!-- Dialog Box-->
-  <div class="dialog" id="myform">
-    <form>
-      <label id="valueFromMyButton">
-      </label>
-      <input type="text" id="name">
-      <div align="center">
-        <input type="button" value="Ok" id="btnOK">
-      </div>
-    </form>
-  </div>
-</div>
-<script>
-$(function() {
-    $(".button").click(function() {
-        $("#myform #valueFromMyButton").text($(this).val().trim());
-        $("#myform input[type=text]").val('');
-        $("#myform").show(500);
-    });
-    $("#btnOK").click(function() {
-        $("#valueFromMyModal").val($("#myform input[type=text]").val().trim());
-        $("#myform").hide(400);
-    });
-});
-</script>
 <style>
-.button{
-  border:1px solid #333;
-  background:#6479fd;
+#overlay {
+     visibility: hidden;
+     position: absolute;
+     left: 0px;
+     top: 0px;
+     width:100%;
+     height:100%;
+     text-align:center;
+     z-index: 1000;
 }
-.button:hover{
-  background:#a4a9fd;
-}
-.dialog{
-  border:5px solid #666;
-  padding:10px;
-  background:#3A3A3A;
-  position:absolute;
-  display:none;
-}
-.dialog label{
-  display:inline-block;
-  color:#cecece;
-}
-input[type=text]{
-  border:1px solid #333;
-  display:inline-block;
-  margin:5px;
-}
-#btnOK{
-  border:1px solid #000;
-  background:#ff9999;
-  margin:5px;
-}
-
-#btnOK:hover{
-  border:1px solid #000;
-  background:#ffacac;
+#overlay div {
+     width:300px;
+     margin: 100px auto;
+     background-color: #fff;
+     border:1px solid #000;
+     padding:15px;
+     text-align:center;
 }
 </style>
+<div id="overlay">
+     <div>
+          <p>Content you want the user to see goes here.</p>
+     </div>
+</div>
+<script>
+function overlay() {
+  el = document.getElementById("overlay");
+  el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+</script>
+
 </html>
