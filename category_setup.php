f0a56d215a49bac2ea0cedb26064878c96931a11
HSchueler <buluen@gmail.com>
2014-04-21 09:14:46 -0700

    cat add final

--- category_setup.php
+++ category_setup.php
@@ -32,7 +32,7 @@
 			if (xhr.readyState == 4) {
 				if (xhr.status == 200) {
 					document.getElementById("catresponse").innerHTML = xhr.responseText;
-					// window.navigate("userhome.php");
+					self.location="userhome.php";
 				} else {
 					alert('There was a problem with the request.');
 				}
