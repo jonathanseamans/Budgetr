932772e916e2a0505e6f9f13b4170d9297cd1e91
HSchueler <buluen@gmail.com>
2014-04-22 16:02:15 -0700

    testszxc234

--- transaction_1.php
+++ transaction_1.php
@@ -5,33 +5,42 @@ session_start();
 $uuid = $_SESSION['userid'];
 ?>
 <body>
-<p>Select the name of your category you wish to add a transaction <select id="ttitle">
+<p>Select the name of your category you wish to add a transaction 
 
-			</select>
+<select id="ttitle">
+<?php 
+	include 'mysql.php';
+	$sql = mysql_query("SELECT UDT FROM budget WHERE uid = '$uuid' AND type = 1");
+	while ($row = mysql_fetch_array($sql)){
+	echo "<option value=\"". $row['UDT'] . "\">" . $row['UDT'] . "</option>";
+	}
+?>
+</select>
 <br>
-<p>Adding or Subtracting? <select>
+<p>Adding or Subtracting? <select id="math">
 		<option value="add">Adding</option>
 		<option value="sub">Subtracting</option>
 	</select></p>
 <p>Enter the amount in this Transaction <input type="number" name="Transaction Title" value=""
 															  id="tvalue"></p>
 <br>
-<button onclick="cat_submit()">Save</button>
-<div id='catresponse'/>
+<button onclick="t_submit()">Save</button>
+<div id='tresponse'/>
 </body>
 </html>
 <script>
-	function cat_submit()
+	function t_submit()
 	{
 		var ttitle = document.getElementById("ttitle").value;
 		var tvalue = document.getElementById("tvalue").value;
+		var math = document.getElementById("math").value;
 		var xhr;
 		if (window.XMLHttpRequest) { // Mozilla, Safari, ...
 			xhr = new XMLHttpRequest();
 		} else if (window.ActiveXObject) { // IE 8 and older
 			xhr = new ActiveXObject("Microsoft.XMLHTTP");
 		}
-		var data = "ttitle=" + ctitle + "&tvalue=" + cvalue;
+		var data = "ttitle=" + ttitle + "&tvalue=" + tvalue + "&math=" + math;
 		xhr.open("POST", "transaction_2.php", true);
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		xhr.send(data);
@@ -39,7 +48,7 @@ $uuid = $_SESSION['userid'];
 		function display_data() {
 			if (xhr.readyState == 4) {
 				if (xhr.status == 200) {
-					document.getElementById("catresponse").innerHTML = xhr.responseText;
+					document.getElementById("tresponse").innerHTML = xhr.responseText;
 					self.location="userhome.php";
 				} else {
 					alert('There was a problem with the request.');
--- transaction_2.php
+++ transaction_2.php
@@ -1,20 +1,27 @@
 <?php
 include 'mysql.php';
 session_start();
-$ttitle = $_POST['ctitle'];
-$tvalue = $_POST['cvalue'];
+$ttitle = $_POST['ttitle'];
+$tvalue = $_POST['tvalue'];
+$math = $_POST['math'];
 $uuid = $_SESSION['userid'];
 
-//$sql = "SELECT bid FROM budget WHERE uid = '$uuid' ORDER BY bid DESC";
-//$result = mysql_query($sql);
-//$num_rows = mysql_num_rows($result);
-//
-//$row = mysql_fetch_array($result);
-//
-//$numbid = $row['bid'] + 1;
-//
+$sql = "SELECT bid FROM budget WHERE uid = '$uuid' ORDER BY bid DESC";
+$result = mysql_query($sql);
+$num_rows = mysql_num_rows($result);
+
+$row = mysql_fetch_array($result);
+
+$numbid = $row['bid'] + 1;
+
+$sql = "SELECT CUDV FROM budget WHERE uid = '$uuid' AND UDT = '$ttitle'";
+$result = mysql_query($sql);
+$row = mysql_fetch_array($result);
 //$sql = "INSERT INTO budget (uid,bid,type,UDT,UDV) VALUES ('$uuid','$num_rows','3','$ctitle','$cvalue')";
 //mysql_query($sql);
 flush();
-echo "Transaction Saved";
+echo $row[0];
+// echo $ttitle;
+// echo $tvalue;
+// echo $math;
 ?>
