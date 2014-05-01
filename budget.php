d7abca9e78eb42f1c6b9c651fe75954947c770a4
HSchueler <buluen@gmail.com>
2014-05-01 15:23:21 -0700

    registration redux

--- budget.php
+++ budget.php
@@ -1,30 +1,4 @@
 <html>
-<style>
-body {
-	padding-top: 50px;
-}
-</style>
-<body>
- <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
 <?php
 	include 'mysql.php';
 	$var = $_SESSION['userid'];		
@@ -56,7 +30,4 @@ body {
 		    }
 	?>
 </p>
- <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
- <script src="assets/js/bootstrap.min.js"></script>
-</body>
 </html>
