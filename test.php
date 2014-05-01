6f0a73129b57f18f5acd89be6d11c6fe2de8b540
HSchueler <buluen@gmail.com>
2014-05-01 08:45:45 -0700

    registration redux

--- test.php
+++ test.php
@@ -1,96 +1,58 @@
-<style type="text/css">
-label.valid {
-width: 24px;
-height: 24px;
-background: url(../img/valid.png) center center no-repeat;
-display: inline-block;
-text-indent: -9999px;
+<html>
+<head>
+<title>User Registration Using PHP Ajax</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$("#submit").click(function(){
+var username=$('#username').val();
+var password=$('#password').val();
+var password1=$('#password1').val();
+var email=$('#email').val();
+$.ajax({
+type: "POST",
+url: "test2.php",
+data: "username="+username+"&password="+password+"&email="+email ,
+success: function(html){
+$("#load").css('display','block');
+$("#form2").css('display','none');
+$("#box").css('display','none');
+$("#load").fadeOut('500', function(){
+$("#load").css('display','none');
+$("#box").html(html).show('slow');
+});
 }
-label.error {
-font-weight: bold;
-padding: 2px 8px;
-margin-top: 2px;
+});
+return false;
+
+});
+});
+</script>
+</head>
+<style type="text/css">
+#load
+{
+display:none;
+width:500px;
+height:500px;
+background:url(loading3.gif) no-repeat;
+}
+#line
+{
+margin:20px 0;
 }
 </style>
-<html>
-<div class="container-fluid">
-<div class="row-fluid">
-<div class="span9 offset3">
-<form action="" id="contact-form" class="form-horizontal" method="post">
-<fieldset>
-    <div class="control-group">
-        <label class="control-label" for="name">Your Name</label>
-        <div class="controls">
-            <input type="text" class="input-xlarge" name="name" id="name">
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="email">Email Address</label>
-<div class="controls">
-            <input type="text" class="input-xlarge" name="email" id="email">
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="subject">Subject</label>
-        <div class="controls">
-            <input type="text" class="input-xlarge" name="subject" id="subject">
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="message">Your Message</label>
-        <div class="controls">
-            <textarea class="input-xlarge" name="message" id="message" rows="3"></textarea>
-        </div>
-    </div>
-    <div class="control-group">
-        <div class="controls">
-            <button type="submit" class="btn">Send Email</button>
-        </div>
-    </div>
-</fieldset>
+<body>
+<div id="load" style="">
+</div>
+<div id="box">
+</div>
+<form method="post" action="" id="form2">
+<div id="line">USERNAME: <input type="text" name="username" id="username" /></div>
+<div id="line">PASSWORD:<input type="password" name="password" id="password" /></div>
+<div id="line">EMAIL:<input type="text" name="email" id="email" /></div>
+<input type="submit" id="submit" name="submit" />
 </form>
-<!-- We will output the results from process.php here -->
-<div id="results"></div>
-</div>
-</div>
-</div>
+
+</body>
 </html>
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-<script>
-$(document).ready(function() {
- $('#contact-form').validate({
-  rules: {
-   name: {
-    minlength: 2,
-    required: true
-   },
-   email: {
-    required: true,
-    email: true
-   },
-   subject: {
-    minlength: 2,
-    required: true
-   },
-   message: {
-    minlength: 2,
-    required: true
-   }
-  },
-  highlight: function(element) {
-   $(element).closest('.control-group').removeClass('success').addClass('error');
-  },
-  success: function(element) {
-   element.text('OK!').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
-  },
-  submitHandler: function(form) {
-   // do other stuff for a valid form
-   $.post('test2.php', $("#contact-form").serialize(), function(data) {
-    $("#contact-form").hide();
-    $('#results').html(data);
-   });
-  }
- });
-}); // end document.ready
-</script>
--- test2.php
+++ test2.php
@@ -1,4 +1,16 @@
-//process.php
 <?php
- print "Form submitted successfully: <br>Your name is <b>".$_POST['name']."</b> and your email is <b>".$_POST['email']."</b><br>";
+include 'mysql.php';
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']))
+{
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$email=$_POST['email'];
+	$query2=mysql_query("SELECT * FROM user");
+	$res = mysql_num_rows($query2);
+	echo $res;
+	if($query2)
+	{
+	echo "<h2>Your Registration Process succesfully completed. Thank You</h2>";
+	}
+}
 ?>
