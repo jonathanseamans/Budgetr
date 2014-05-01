c86af14697abbff7f2331a15702ac27dacfd0b26
HSchueler <buluen@gmail.com>
2014-05-01 15:41:15 -0700

    registration redux

--- userhome.php
+++ userhome.php
@@ -3,19 +3,18 @@
  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="userhome.php">BUDGETR</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="userhome.php">Home</a></li>
+            <li><a href="category_setup.php">Categories</a></li>
+            <li><a href="transaction_1.php">Transactions</a></li>
+            <li><a href="logout.php">Logout</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -26,10 +25,6 @@ if($_SESSION['loggedIn'] == false) {
 	header("location:index.php");
 }
 ?>
-	<span id="logo">BUDGETR</span> <span id="zheader">
-		<?php echo "You are logged in as ". $_SESSION['user']." ";?><a href="logout
-		.php"><button>Logout</button></a>
-	</span>
 <head>
 </head>
 <body>
