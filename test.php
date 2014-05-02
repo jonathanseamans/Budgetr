6e3dc8e0bf08ec9e521b3b46729df0c7f328529a
HSchueler <buluen@gmail.com>
2014-05-01 20:32:01 -0800

    bootstrappy

--- test.php
+++ test.php
@@ -4,7 +4,22 @@
      <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
    <body>
-   <h1>Hello, world!</h1>
+   <div class="row"><!-- span numbers must add up to 12 --> 
+	<div class="span6">...</div>
+	<div class="span6">...</div>
+	</div>
+	<div class="row"><!-- span numbers must add up to 12 --> 
+	<div class="span4">...</div>
+	<div class="span8">...</div>
+	</div>
+	<div class="row"><!-- span numbers must add up to 12 --> 
+	<div class="span4">...</div>
+	<div class="span4">...</div>
+	<div class="span4">...</div>
+	</div>
+	<div class="row"><!-- span numbers must add up to 12 --> 
+	<div class="span12">...</div>
+	</div>
    <script src="js/bootstrap.min.js"></script>
    </body>
 </html>
