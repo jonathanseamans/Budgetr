5f832e1cc1d3d21c34854b88d735dcea13bcc121
HSchueler <buluen@gmail.com>
2014-05-01 20:49:17 -0800

    bootstrappy

--- category_display.php
+++ category_display.php
@@ -1,15 +1,6 @@
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
-<style>
-	#content{
-		color: rgb(255,255,255);
-}
-table {
-	color: rgb(255,255,255);
-}
-
-</style>
 <div id="content">
 	<?php
 	$uuid = $_SESSION['userid'];
@@ -23,14 +14,15 @@ table {
 	$result = mysql_query("SELECT * FROM budget WHERE uid=$uuid AND type=1");
 //	$row = mysql_fetch_assoc($result);
 //	$count=count($row);
+	?>
 
-	echo "<table class="table table-striped" width="647">
-<tr>
-<th>&nbsp;Category&nbsp;</th>
-<th>&nbsp;Budgeted Amount&nbsp;</th>
-<th>&nbsp;Current Amount&nbsp;</th>";
+	<table class="table table-striped" width="647">
+	<tr>
+	<th>&nbsp;Category&nbsp;</th>
+	<th>&nbsp;Budgeted Amount&nbsp;</th>
+	<th>&nbsp;Current Amount&nbsp;</th>";
 
-
+	<?php
 	while($row = mysql_fetch_array($result))
   	{
 		  echo "<tr>";
