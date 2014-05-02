6e6348730cf1b9888de6e91acc77461642fbb9e2
HSchueler <buluen@gmail.com>
2014-05-01 20:39:01 -0800

    bootstrappy

--- test.php
+++ test.php
@@ -1,25 +1,37 @@
+
+
 <html>
-    <head>
-     <title>Basic Bootstrap Template</title>
-     <link href="css/bootstrap.min.css" rel="stylesheet">
-    </head>
-   <body>
-   <div class="row"><!-- span numbers must add up to 12 --> 
-	<div class="span6">...</div>
-	<div class="span6">...</div>
-	</div>
-	<div class="row"><!-- span numbers must add up to 12 --> 
-	<div class="span4">...</div>
-	<div class="span8">...</div>
-	</div>
-	<div class="row"><!-- span numbers must add up to 12 --> 
-	<div class="span4">...</div>
-	<div class="span4">...</div>
-	<div class="span4">...</div>
-	</div>
-	<div class="row"><!-- span numbers must add up to 12 --> 
-	<div class="span12">...</div>
-	</div>
-   <script src="js/bootstrap.min.js"></script>
-   </body>
-</html>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<table class="table table-striped" width="647">
+    <thead>
+    <tr>
+    <th>#</th>
+    <th>Name</th>
+    <th>Address</th>
+    <th>mail</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+    <td>1</td>
+    <td>Thomas bell</td>
+    <td>Brick lane, London</td>
+    <td>thomas@yahoo.com</td>
+    </tr>
+    <tr>
+    <td height="29">2</td>
+    <td>Yan Chapel</td>
+    <td>Toronto Australia</td>
+    <td>Yan@yahoo.com</td>
+    </tr>
+    <tr>
+    <td>3</td>
+    <td>Pit Sampras</td>
+    <td>Berlin, Germany</td>
+    <td>Pit @yahoo.com</td>
+    </tr>
+    </tbody>
+    </table>
+    </html>
