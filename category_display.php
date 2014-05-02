5aed6db6f3109fee6efbd007a8840791bc0f14ba
HSchueler <buluen@gmail.com>
2014-05-02 09:51:18 -0700

    fixes

--- category_display.php
+++ category_display.php
@@ -1,33 +1,35 @@
-<style>
-	#content{
-		color: rgb(255,255,255);
-}
-th {
-	color: rgb(255,255,255);
-}
-
-</style>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<style>
+		#content {
+			background-color: rgba(2,2,2,.5);
+			color: rgb(255,255,255);
+		}
+		#tabs {
+			background-color: rgba(2,2,2,.5);
+			color: rgb(255,255,255);
+		}
+		.table-hover tbody tr:hover td, .table-hover tbody tr:hover th {
+ 		 background-color: rgb(50,0,0);
+		}
+		</style>
 <div id="content">
 	<?php
 	$uuid = $_SESSION['userid'];
 	$result = mysql_query("SELECT UDT FROM budget WHERE uid = $uuid AND type = 2");
 	$row = mysql_fetch_row($result);
-	echo $row[0]."<br>";
-	?>
-	<br>
+	echo '<center> <B> <font size="5">'.$row[0];
+
+	$result = mysql_query("SELECT * FROM budget WHERE uid=$uuid AND type=1");?>
+
+	<table class="table table-hover" id="tabs" width="400">
+	<tr>
+	<th>&nbsp;Category&nbsp;</th>
+	<th>&nbsp;Budgeted Amount&nbsp;</th>
+	<th>&nbsp;Current Amount&nbsp;</th>
+
 	<?php
-
-	$result = mysql_query("SELECT * FROM budget WHERE uid=$uuid AND type=1");
-//	$row = mysql_fetch_assoc($result);
-//	$count=count($row);
-
-	echo "<table class="table table-striped" border='1'>
-<tr>
-<th>&nbsp;Category&nbsp;</th>
-<th>&nbsp;Budgeted Amount&nbsp;</th>
-<th>&nbsp;Current Amount&nbsp;</th>";
-
-
 	while($row = mysql_fetch_array($result))
   	{
 		  echo "<tr>";
@@ -39,7 +41,4 @@ th {
 	echo "</table>";
 
 	?>
-	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/docs.min.js"></script>
 </div>
--- transaction_1.php
+++ transaction_1.php
@@ -1,10 +1,42 @@
 <html>
-<style><?php include 'css/main.css'; ?></style>
+<style>
+body
+{
+    background: url(resources/unnamed.png) ;
+    color: rgb(255,255,255);
+    padding: 50px;
+}
+p {
+	color: rgb(255,255,255);
+}
+</style>
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+ <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="userhome.php">BUDGETR</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="userhome.php">Home</a></li>
+            <li><a href="category_setup.php">Categories</a></li>
+            <li class="active"><a href="transaction_1.php">Transactions</a></li>
+            <li><a href="edit_title.php">Options</a></li>
+            <li><a href="logout.php">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 <?php
 session_start();
 $uuid = $_SESSION['userid'];
 ?>
 <body>
+<br>
 <p>Select the name of your category you wish to add a transaction 
 
 <select id="ttitle">
@@ -59,3 +91,5 @@ $uuid = $_SESSION['userid'];
 		}
 	}
 </script>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
